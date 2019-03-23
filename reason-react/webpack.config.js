f1365daf2ac60899883e1cb56a987044c789e11e
Amélie <amelie.benoit33@gmail.com>
2019-03-23 15:34:44 -0400

    Change webpack configuration

--- reason-react/webpack.config.js
+++ reason-react/webpack.config.js
@@ -1,27 +1,19 @@
-const path = require("path");
+const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const outputDir = path.join(__dirname, "build/");
-
-const isProd = process.env.NODE_ENV === "production";
+const path = require("path");
 
 module.exports = {
-  entry: "./src/Index.bs.js",
-  mode: isProd ? "production" : "development",
-  output: {
-    path: outputDir,
-    publicPath: outputDir,
-    filename: "Index.js"
+  mode: "development",
+  entry: path.resolve(__dirname, "src/Index.bs.js"),
+  watch: true,
+  devServer: {
+    port: 8000,
+    hot: true
   },
   plugins: [
+    new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
-      template: "build/index.html",
-      inject: false
+      template: path.resolve(__dirname, "src/index.html")
     })
-  ],
-  devServer: {
-    compress: true,
-    contentBase: outputDir,
-    port: process.env.PORT || 8000,
-    historyApiFallback: true
-  }
+  ]
 };
